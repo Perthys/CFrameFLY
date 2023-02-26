b6560545c42afe07cffe8f89a55682031c5f59ff
Jason Jack Tan <52866402+Perthys@users.noreply.github.com>
2023-02-25 18:29:12 -0800

    Update cframefly.lua

--- cframefly.lua
+++ cframefly.lua
@@ -16,7 +16,36 @@ local Character = LocalPlayer.Character or LocalPlayer.Character or LocalPlayer.
 
 local CurrentPoint = Character:GetPivot();
 
+local task = table.clone(task);
+
+local OldDelay = task.delay;
+
+function task.delay(Time, Function)
+    local Enabled = true;
+    
+    OldDelay(Time, function()
+        if Enabled then
+            Function();
+        end
+    end)
+    
+    return {
+        Cancel = function()
+            Enabled = false;
+        end;
+        Activate = function()
+            Enabled = false
+            Function()
+        end
+    }
+end
 local wait = task.wait;
+
+local function StopVelocity()
+    local HumanoidRootPart = Character and Character:FindFirstChild("HumanoidRootPart"); if not HumanoidRootPart then return end;
+    
+    HumanoidRootPart.Velocity = Vector3.zero;
+end
 
 Maid:GiveTask(LocalPlayer.CharacterAdded:Connect(function(NewCharacter)
     Character = NewCharacter
@@ -24,12 +53,15 @@ end))
 
 Maid:GiveTask(RunService.Stepped:Connect(function()
     if shared.Active then
+        StopVelocity();
         local CameraCFrame = Camera.CFrame
         
         CurrentPoint = CFrame.new(CurrentPoint.Position, CurrentPoint.Position + CameraCFrame.LookVector)
         Character:PivotTo(CurrentPoint);
     end
 end))
+
+local CurrentTask = nil;
 
 local KeyBindStarted = {
     [Enum.KeyCode.Q] = {
@@ -88,13 +120,44 @@ local KeyBindStarted = {
     };
     [Enum.KeyCode.Equals] = {
         ["TOGGLE"] = function()
+            local Humanoid = Character:FindFirstChild("Humanoid") if not Humanoid then return end;
+            local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart") if not HumanoidRootPart then return end;
+            
             if not shared.Active then
                 CurrentPoint = Character:GetPivot();
+            else
+                if CurrentTask then
+                    CurrentTask:Activate()
+                end
+            
+                StopVelocity();
+                
+                Character:PivotTo(CFrame.new(Character:GetPivot().Position))
+                
+                local RunServiceSignal = RunService.Stepped:Connect(function()
+                    local AssemblyAngularVelocity = HumanoidRootPart.AssemblyAngularVelocity;
+                    
+                    if AssemblyAngularVelocity.X > 20 
+                    or AssemblyAngularVelocity.Y > 20
+                    or AssemblyAngularVelocity.Z > 20  then
+                         Character:PivotTo(CFrame.new(Character:GetPivot().Position))
+                    end
+                end)
+                
+                CurrentTask = task.delay(10, function()
+                    RunServiceSignal:Disconnect()
+                end)
+                
+                Maid:GiveTask(RunServiceSignal)
             end
             
             shared.Active = not shared.Active
         end;
     }
+}
+
+local KeyBindEnded = {
+   
 }
 
 Maid:GiveTask(UserInputService.InputBegan:Connect(function(Input, GameProcessedEvent)
@@ -105,7 +168,21 @@ Maid:GiveTask(UserInputService.InputBegan:Connect(function(Input, GameProcessedE
 			end
 		elseif KeyBindStarted[Input.UserInputType] then
 			for _, Function in pairs(KeyBindStarted[Input.UserInputType]) do
-                		task.spawn(Function)
+                task.spawn(Function)
+			end
+		end
+	end
+end))
+
+Maid:GiveTask(UserInputService.InputEnded:Connect(function(Input, GameProcessedEvent)
+	if not GameProcessedEvent then
+		if Input.UserInputType == Enum.UserInputType.Keyboard and KeyBindEnded[Input.KeyCode] then
+			for _, Function in pairs(KeyBindEnded[Input.KeyCode]) do
+				task.spawn(Function)
+			end
+		elseif KeyBindEnded[Input.UserInputType] then
+			for _, Function in pairs(KeyBindEnded[Input.UserInputType]) do
+                task.spawn(Function)
 			end
 		end
 	end
